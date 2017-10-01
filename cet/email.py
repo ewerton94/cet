6eaeddfc4b9390a951aa633691692d22ddda5f92
Ewerton Oliveira <ewerton.amorim@live.com>
2017-09-30 22:14:47 -0300

    edit setting agains

--- cet/email.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'petct.ufal@outlook.com'
-EMAIL_HOST_PASSWORD = 'ct32141871'
